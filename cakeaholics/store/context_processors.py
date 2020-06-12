b3e179847a2d54477c934db9db1fad02d82b8ae3
x NeWIzZ x SD 39 <newkmitl47@gmail.com>
2020-06-12 21:13:58 +0700

    Show filter products by category

--- cakeaholics/store/context_processors.py
+++ cakeaholics/store/context_processors.py
@@ -1,6 +1,6 @@
 from store.models import Category
 from django.urls import reverse
 
-def menu_link(request):
+def menu_links(request):
     links=Category.objects.all()
     return dict(links=links)
